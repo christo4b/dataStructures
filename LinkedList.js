b8f074d9fc53d2c5c95cab57b564609bd2fb2f17
Chris Bassano <christo4b@gmail.com>
2017-02-15 20:15:50 -0800

    lru cache set up with internal properties

--- LinkedList.js
+++ LinkedList.js
@@ -7,14 +7,14 @@
 
 // EXAMPLE USAGE:
 // var list = new LinkedList();
-// list.tail;         //yields 'null'
-// list.addToTail(4);
-// list.addToTail(5);
-// list.head.value;   //yields '4';
-// list.contains(5);  //yields 'true';
-// list.contains(6);  //yields 'false';
-// list.removeHead(); //yields '4'
-// list.tail.value;   //yields '5';
+// console.log(list.tail;         )//yields 'null'
+// console.log(list.addToTail(4);)
+// console.log(list.addToTail(5);)
+// console.log(list.head.value;   )//yields '4';
+// console.log(list.contains(5);  )//yields 'true';
+// console.log(list.contains(6);  )//yields 'false';
+// console.log(list.removeHead(); )//yields '4'
+// console.log(list.tail.value;   )//yields '5';
 
 var LinkedList = function(){
   this.head = null
@@ -25,7 +25,6 @@ var ListNode = function(value, next){
   this.value = value
   this.next = next || null
 }
-
 
 LinkedList.prototype.addToTail = function(value){
   // empty list
@@ -48,6 +47,25 @@ LinkedList.prototype.removeHead = function(){
   return current
 }
 
-LinkedList.prototype.contains = function(){}
+LinkedList.prototype.contains = function(value){
+  let node = this.head
+  if (this.head.value === value || this.tail.value === value) {
+    return true
+  }
+  while (node){
+    if (node.value === value) return true;
+    node = node.next
+  }
+  return false
+}
 
-LinkedList.prototype.removeNode = function(){}
+LinkedList.prototype.removeNode = function(node){
+  // given a pointer to a node, we can effectively remove it
+  if (this.tail === node){
+    this.removeTail();
+    return node.value
+  }
+
+  node.value = node.next.value
+  node.next=node.next.next
+}
